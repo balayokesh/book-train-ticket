d37029d96852de1b6b3eebd8a15cec8a529b0333
yokesh004 <balayokeshmani@gmail.com>
2021-04-19 16:54:09 +0530

    implemented server, mongoose connection

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const app = express();
+const uri = 'mongodb+srv://username:strongpassword@train.ebtm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.use(cors());
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Mongoose connection success');
+})
+
+app.listen(port, () => {
+    console.log(`Server connection success at port ${port}`);
+});
