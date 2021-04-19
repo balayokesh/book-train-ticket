cdb66c00d6b548d11cd035d9c15f832696fa8abe
yokesh004 <balayokeshmani@gmail.com>
2021-04-19 18:21:55 +0530

    rectified json body undefined error

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(express.json());
+app.use(bodyParser.json())
+
+// Mongoose connection
 const uri = 'mongodb+srv://username:strongpassword@train.ebtm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app.use(cors());
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -12,6 +17,11 @@ connection.once('open', () => {
     console.log('Mongoose connection success');
 })
 
+// Routes
+const trains = require('./routes/trains');
+app.use('/trains', trains);
+
+// Run server
 app.listen(port, () => {
     console.log(`Server connection success at port ${port}`);
 });
