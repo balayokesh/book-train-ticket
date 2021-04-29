b6ac9acf0b2fa381bbf75aecf827bf7915d2ed7b
yokesh004 <balayokeshmani@gmail.com>
2021-04-30 01:43:59 +0530

    Added routes for ticket management

--- server/routes/trains.js
+++ server/routes/trains.js
@@ -1,12 +1,70 @@
 const router = require('express').Router();
+
+// Models used in this router
 const Trains = require('../models/trains.model');
 const TrainAvailability = require('../models/trainAvailability.model');
 
-// Get all trains
+/*
+    Existing Routes:
+    /trains - fetch all from trains table
+    /trains/add - add new document to trains table
+    /trains/add/seats - add new document to trainAvailability table
+    /trains/:id/availability - fetches all classes available in that particular train id
+    /trains/:id/reservation - increments certain trains classes availability.reserved count
+    /trains/:id/cancellation - decrements certain trains classes availability.reserved count
+*/
+
 router.route('/').get((req, res) => {
     Trains.find()
         .then(trains => res.json(trains))
         .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/add').post((req, res) => {
+    const train_num = req.body.train_num;
+    const train_name = req.body.train_name;
+
+    newTrain = new Trains({
+        train_num, train_name
+    });
+
+    newTrain.save()
+        .then(() => res.json(`Train ${train_name} information added successfully`))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/addseats').post((req, res) => {
+    const train_id = req.body.train_id;
+    const train_class = req.body.train_class;
+    const availability = req.body.availability;
+
+    trainDetails = new TrainAvailability ({
+        train_id, train_class, availability
+    });
+
+    trainDetails.save()
+        .then(() => res.json(`Train ${train_id} details updation success.`))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id/availability').get((req, res) => {
+    TrainAvailability.find({train_id: req.params.id})
+        .then((availabilities) => res.json(availabilities))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id/reservation').post((req, res) => {
+    const class_id = req.body.class_id;
+    TrainAvailability.findOneAndUpdate({_id: class_id}, {$inc: {"availability.reserved": 1 }})
+        .then(() => res.json(`You booked class: ${class_id} at train ${req.params.id}`))
+        .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id/cancellation').post((req, res) => {
+    const class_id = req.body.class_id;
+    TrainAvailability.findOneAndUpdate({_id: class_id}, {$inc: {"availability.reserved": -1 }})
+        .then(() => res.json(`You booked class: ${class_id} at train ${req.params.id}`))
+        .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 router.route('/search').get((req, res) => {
@@ -60,38 +118,10 @@ router.route('/load-route').get((req, res) => {
     }
 });
 
-router.route('/add').post((req, res) => {
-    const train_num = req.body.train_num;
-    const train_name = req.body.train_name;
-
-    newTrain = new Trains({
-        train_num, train_name
-    });
-
-    newTrain.save()
-        .then(() => res.json(`Train ${train_name} information added successfully`))
-        .catch(err => res.status(400).json(`Error: ${err}`));
-});
-
 router.route('/:id').get((req, res) => {
     Trains.findById(req.params.id)
         .then(train => res.json(train))
         .catch(err => res.status(400).json(`Error: ${err}`));
 });
-
-// Add ticket availability for different classes
-router.route('/addseats').post((req, res) => {
-    const train_id = req.body.train_id;
-    const train_class = req.body.train_class;
-    const availability = req.body.availability;
-
-    trainDetails = new TrainAvailability ({
-        train_id, train_class, availability
-    });
-
-    trainDetails.save()
-        .then(() => res.json(`Train ${train_id} details updation success.`))
-        .catch(err => res.status(400).json(`Error: ${err}`));
-})
 
 module.exports = router;
