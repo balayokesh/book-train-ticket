fa64e01dfc3db4061bda8d199ba475875f847cef
yokesh004 <balayokeshmani@gmail.com>
2021-05-03 15:12:45 +0530

    implemetned createJWT function for util

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const createJWT = (email, userId, duration) => {
+    const payload = {
+        email,
+        userID,
+        duration
+    };
+    return jwt.sign(
+        payload, 
+        process.env.TOKEN_SECRET, 
+        { expiresIn: duration }
+    );
+}
